28b3377dd311b0793b8348d0929fbdbf9f56240a
Damyan <damyanbsantanderh@gmail.com>
2025-09-09 17:31:25 -0300

    shaders

--- js/Run_Compute_Shader.js
+++ js/Run_Compute_Shader.js
@@ -29,7 +29,7 @@ export function runComputeShader(device, commandEncoder, uniformBuffer, uniforms
 // This asynchronous function will retrieve the shader source code as text.
 export async function fetchShader(url) {
     // Use the Fetch API to get the resource at the specified URL.
-    const response = await fetch(url);
+    const response = await fetch("./"+url);
 
     // Once we get the response, retrieve the text from it.
     return await response.text();
